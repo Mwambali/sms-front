fdf1344408cbd8f8188cf6df058cb2dcefdc1dd6
Mwambali <ishimwesion@gmail.com>
2023-06-13 20:56:23 +0200

    tweaks

--- src/store/actions/errorActions.ts
+++ src/store/actions/errorActions.ts
@@ -17,29 +17,3 @@ export const clearErrors = () => {
         type: 'error/clearErrors',
     };
 };
-
-
-
-// import { GET_ERRORS, CLEAR_ERRORS } from "./types";
-
-// interface ErrorPayload {
-//     msg: string;
-//     status: number;
-//     id: string | null;
-// }
-
-// // RETURN ERRORS
-// export const returnErrors = (msg: string, status: number, id: string | null = null) => {
-//     const payload: ErrorPayload = { msg, status, id };
-//     return {
-//         type: GET_ERRORS,
-//         payload,
-//     };
-// };
-
-// // CLEAR ERRORS
-// export const clearErrors = () => {
-//     return {
-//         type: CLEAR_ERRORS,
-//     };
-// };
